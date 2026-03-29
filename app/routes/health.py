39064ac421640f7774676916cfa4a33f658c8dc2
am499702@gmail.com <am499702@gmail.com>
2026-03-29 22:19:43 +0530

    Added the DB Call for cron jon

--- app/routes/health.py
+++ app/routes/health.py
@@ -1,8 +1,23 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response,Depends
+from requests import Session
+from sqlalchemy import text
+
+from app.db.database import get_db
 router = APIRouter()
+
 @router.get("/health")
 def health():
     return {"status": "ok"}
+
 @router.head("/health")
 def health_head():
+    return Response(status_code=200)
+
+@router.get("/db-health")
+def db_health(db: Session = Depends(get_db)):
+    db.execute(text("SELECT 1"))
+    return {"status": "ok"}
+
+@router.head("/db-health")
+def db_health_head():
     return Response(status_code=200)
